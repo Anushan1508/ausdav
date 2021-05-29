d08d8f86f0a48d41c7ad4da72b244088e31718a2
anushan1508 <anushan1508@gmail.com>
2021-05-29 16:59:43 +0530

    style nav

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from 'react';
+import { FaBars } from 'react-icons/fa';
+import { 
+    Nav, 
+    NavbarContainer, 
+    NavLogo, 
+    MobileIcon, 
+    NavMenu, 
+    NavItem, 
+    NavLinks
+} from './NavbarElements';
 
 const Navbar = () => {
     return (
         <div>
-            <h1>Navbar</h1>
+            <Nav>
+                <NavbarContainer>
+                    <NavLogo to="/" >AUSDAV</NavLogo>
+                    <MobileIcon>
+                        <FaBars />
+                    </MobileIcon>
+                    <NavMenu>
+                        <NavItem>
+                            <NavLinks to="about">About</NavLinks>
+                        </NavItem>
+                    </NavMenu>
+                </NavbarContainer>
+            </Nav>
         </div>
     )
 }
